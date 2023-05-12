778c39292dab2d9aeff4aa3df6d8f8c2d6ca1c37
João Felipe Zini <joaofelipezini@gmail.com>
2023-05-12 14:50:08 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom/cjs/react-router-dom';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <Switch>
+      <Route exact path="/" component={ Login } />
+    </Switch>
+  );
+}
+
+export default App;
