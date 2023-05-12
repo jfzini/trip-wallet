d8a35d805066619d5831fdfb716d0dc2f15e5d7b
João Felipe Zini <joaofelipezini@gmail.com>
2023-05-12 18:25:54 -0300

    wip req 3

--- src/components/WalletForm.jsx
+++ src/components/WalletForm.jsx
@@ -1,11 +1,49 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { actionGetCurrencies } from '../redux/actions';
 
 class WalletForm extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(actionGetCurrencies());
+  }
+
   render() {
+    const { currencies } = this.props;
     return (
-      <div>WalletForm</div>
+      <form action="">
+        <input data-testid="value-input" type="text" placeholder="valor" />
+        <input data-testid="description-input" type="text" placeholder="descrição" />
+        <select data-testid="currency-input" name="currency" id="currency">
+          {currencies.map((cur) => <option key={ cur } value="currencies">{cur}</option>)}
+        </select>
+        <select name="payment-method" id="payment-method" data-testid="method-input">
+          <option value="payment-method">Dinheiro</option>
+          <option value="payment-method">Cartão de crédito</option>
+          <option value="payment-method">Cartão de débito</option>
+        </select>
+        <select name="tag" id="tag" data-testid="tag-input">
+          <option value="tag">Alimentação</option>
+          <option value="tag">Lazer</option>
+          <option value="tag">Trabalho</option>
+          <option value="tag">Transporte</option>
+          <option value="tag">Saúde</option>
+        </select>
+      </form>
     );
   }
 }
 
-export default WalletForm;
+WalletForm.propTypes = {
+  currencies: PropTypes.shape({
+    map: PropTypes.func,
+  }),
+  dispatch: PropTypes.func,
+}.isRequired;
+
+const mapStateToProps = (globalState) => ({
+  currencies: globalState.wallet.currencies,
+});
+
+export default connect(mapStateToProps)(WalletForm);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,19 @@
 export const SUBMIT_LOGIN = 'SUBMIT_LOGIN';
+export const GET_CURRENCIES = 'GET_CURRENCIES';
 
 export const actionSubmitLogin = (state) => ({
   type: SUBMIT_LOGIN,
   payload: state,
 });
+
+const loadCurrencies = (currencies) => ({
+  type: GET_CURRENCIES,
+  payload: currencies,
+});
+
+export const actionGetCurrencies = () => async (dispatch) => {
+  const fetchCurrencies = await fetch('https://economia.awesomeapi.com.br/json/all');
+  const JSONCurrencies = await fetchCurrencies.json();
+  const currenciesArray = Object.keys(JSONCurrencies).filter((key) => key !== 'USDT');
+  dispatch(loadCurrencies(currenciesArray));
+};
