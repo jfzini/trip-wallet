592dd3d9b9024ad0c8f741a5ca4b25deacf153f4
João Felipe Zini <joaofelipezini@gmail.com>
2023-05-13 10:23:30 -0300

    req 7

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,23 +1,53 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <table>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses
+            .map(({ description, value, method, tag, currency, exchangeRates, id }) => (
+              <tr key={ id }>
+                <td>{description}</td>
+                <td>{tag}</td>
+                <td>{method}</td>
+                <td>{Number(value).toFixed(2)}</td>
+                <td>{exchangeRates[currency].name}</td>
+                <td>{(exchangeRates[currency].ask * 1).toFixed(2)}</td>
+                <td>{(exchangeRates[currency].ask * value).toFixed(2)}</td>
+                <td>Real</td>
+                <td>Editar/Excluir</td>
+              </tr>))}
+        </tbody>
       </table>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  expenses: PropTypes.shape({
+    map: PropTypes.func,
+  }),
+}.isRequired;
+
+const mapStateToProps = (globalState) => ({
+  expenses: globalState.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
